7a29e7280e95d8e4d4a1ff21252040c0fc77dd94
Yan Wallas <yanwallas98@gmail.com>
2025-04-23 17:30:21 -0300

    trazendo orders nao finalizadas

--- src/app/dashboard/requests/page.tsx
+++ src/app/dashboard/requests/page.tsx
@@ -1,12 +1,19 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./styles.module.scss";
 import { Button } from "../components/button";
 import { getCookieClient } from "@/lib/cookieClient";
 import { api } from "@/services/api";
 import { toast } from "sonner";
 
+interface Order{
+  id: string;
+  table: number;
+  name?: string;
+}
+
 export default function Requests() {
+  const [orders, setOrders] = useState<Order[]>([]);
 
   async function handleRequests(formData: FormData) {
     const table = formData.get("table");
@@ -34,6 +41,27 @@ export default function Requests() {
     toast.success("Pedido criado com sucesso!");
   }
 
+  async function fetchOrders() {
+    try {
+      const token = await getCookieClient();
+
+      const response = await api.get("/orders/open", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      setOrders(response.data);
+
+    } catch (err) {
+      console.log("Erro ao buscar pedidos", err);
+    }
+  }
+
+  useEffect(() => {
+    fetchOrders();
+  },[]);
+
   return (
     <main className={styles.container}>
       <h1>Digite o número da mesa para fazer o pedido.</h1>
@@ -57,7 +85,24 @@ export default function Requests() {
       
         <Button name='Fazer Pedido'/>
       </form>
-      <h1>Pedidos não finalizados.</h1>
+
+      {orders.length === 0 && (
+        <span className={styles.emptyItem}>
+          Nenhum pedido em aberto no momento...
+        </span>
+      )}
+
+      {orders.map((order) => (
+        <button key={order.id} className={styles.orderItem}>
+          <div className={styles.tag}></div>
+          <span>Mesa: {order.table}</span>
+          {order.name ? (
+            <span className={styles.name}>Nome: {order.name}</span>
+          ) : (
+            ""
+          )}
+        </button>
+      ))}
  
     </main>
   );
